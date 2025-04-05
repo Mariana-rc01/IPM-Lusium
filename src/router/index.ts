1312321dbacfc880336883fb28c5e14389adfe07
Eduardo Faria <eduardo21faria@gmail.com>
2025-04-05 19:08:27 +0100

    feat: courses' list page

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,37 +1,44 @@
-import { createRouter, createWebHistory } from 'vue-router'
-import LoginView from '@/views/LoginView.vue'
-import StudentView from '@/views/StudentView.vue'
-import TeacherView from '@/views/TeacherView.vue'
-import TicketsView from '@/views/TicketsView.vue'
+import { createRouter, createWebHistory } from "vue-router";
+import LoginView from "@/views/LoginView.vue";
+import StudentView from "@/views/StudentView.vue";
+import TeacherView from "@/views/TeacherView.vue";
+import TicketsView from "@/views/TicketsView.vue";
+import CourseListView from "@/views/CourseListView.vue";
 
 const router = createRouter({
-    history: createWebHistory(import.meta.env.BASE_URL),
-    routes: [
+  history: createWebHistory(import.meta.env.BASE_URL),
+  routes: [
     {
-        path: '/',
-        redirect: '/login'
+      path: "/",
+      redirect: "/login",
     },
     {
-        path: '/login',
-        name: 'login',
-        component: LoginView
+      path: "/login",
+      name: "login",
+      component: LoginView,
     },
     {
-        path: '/aluno',
-        name: 'student',
-        component: StudentView
+      path: "/aluno",
+      name: "student",
+      component: StudentView,
     },
     {
-        path: '/docente',
-        name: 'teacher',
-        component: TeacherView
+      path: "/docente",
+      name: "teacher",
+      component: TeacherView,
     },
     {
-        path: '/tickets',
-        name: 'tickets',
-        component: TicketsView
-    }
-    ]
-})
+      path: "/tickets",
+      name: "tickets",
+      component: TicketsView,
+    },
+    {
+      path: "/courses",
+      name: "coursesList",
+      component: CourseListView,
+    },
+  ],
+});
 
-export default router
+export default router;
+
