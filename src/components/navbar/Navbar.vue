dc6ec1f86b5eabe78946c7a0a91b816b918fd129
Hélder Gomes <116376171+helderrrg@users.noreply.github.com>
2025-04-15 17:51:02 +0100

    feat: add nav bar (#11)

---
+++ src/components/navbar/Navbar.vue
@@ -0,0 +1,13 @@
+<script setup lang="ts">
+import MainNav from './MainNav.vue';
+</script>
+
+<template>
+    <div class="border-none">
+      <div class="flex h-16 items-center px-5">
+        <MainNav class="mx-6" />
+        <div class="ml-auto flex items-center space-x-4">
+        </div>
+      </div>
+    </div>
+</template>
