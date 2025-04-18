deec7a6cd64ed04939273c7b732b05e348193c53
Mariana <rc.mariana.01@gmail.com>
2025-04-18 22:58:00 +0100

    fix: remove DEBUG

--- src/components/navbar/Navbar.vue
+++ src/components/navbar/Navbar.vue
@@ -1,14 +1,8 @@
 <script setup lang="ts">
 import MainNav from './MainNav.vue';
-import { useUserStore } from '@/stores/user';
-
-const userStore = useUserStore();
 </script>
 
 <template>
-    <div v-if="userStore.user">
-      <p> [DEBUG] {{ userStore.user.name }} é {{ userStore.user.type }}</p>
-    </div>
     <div class="border-none">
       <div class="flex h-16 items-center px-5">
         <MainNav class="mx-6" />
